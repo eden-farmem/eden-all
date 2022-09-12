4c489cd3105674c495bae02ed040a7782a22f1af
Anil Yelam <ayelam@vmware.com>
2022-09-12 15:16:29 -0700

    parsec numbers and script updates

--- scripts/parse_kona_faults.py
+++ scripts/parse_kona_faults.py
@@ -1,16 +1,33 @@
-import os
 import argparse
-import pandas as pd
+from enum import Enum
+import os
 import sys
+
+import pandas as pd
 
 TIMECOL = "time"
 KINDCOL = "kind"
+
+class FaultKind(Enum):
+    READ = "read"
+    WRITE = "write"
+    WRPROTECT = "wrprotect"
+    def __str__(self):
+        return self.value
+
+def kind_to_enum(kind):
+    if kind == 0:   return FaultKind.READ
+    if kind == 1:   return FaultKind.WRITE
+    if kind == 3:   return FaultKind.WRPROTECT
+    raise Exception("unknown kind")
+
 
 def main():
     parser = argparse.ArgumentParser("Process input and write csv-formatted data to stdout/output file")
     parser.add_argument('-i', '--input', action='store', help="path to the input/data file", required=True)
     parser.add_argument('-st', '--start', action='store', type=int,  help='start (unix) time to filter data')
     parser.add_argument('-et', '--end', action='store', type=int, help='end (unix) time to filter data')
+    parser.add_argument('-fk', '--kind', action='store', type=FaultKind, choices=list(FaultKind), help='filter for a specific kind of fault')
     parser.add_argument('-o', '--out', action='store', help="path to the output file")
     args = parser.parse_args()
 
@@ -21,19 +38,20 @@ def main():
     df = pd.read_csv(args.input, skipinitialspace=True) 
 
     # filter 
-    if args.start:  df = df[df[TIMECOL] >= args.start]
-    if args.end:    df = df[df[TIMECOL] <= args.end]
+    if args.start:  
+        df = df[df[TIMECOL] >= args.start]
+    if args.end:    
+        df = df[df[TIMECOL] <= args.end]
     df[TIMECOL] = df[TIMECOL] - df[TIMECOL].iloc[0]
 
-    # rewrite kind
-    def kind_to_str(kind):
-        if kind == 0:   return "read"
-        if kind == 1:   return "write"
-        if kind == 3:   return "wrprotect"
-        raise Exception("unknown kind")
-    df["kind"] = df.apply(lambda r: kind_to_str(r["kind"]), axis=1)
+    # rewrite and filter by kind
+    df["kind"] = df.apply(lambda r: kind_to_enum(r["kind"]).value, axis=1)
+    if args.kind is not None:
+        df = df[df[KINDCOL] == args.kind.value]
     df = df.groupby(['ip', 'kind']).size().reset_index(name='count')
     df = df.sort_values("count", ascending=False)
+    df["percent"] = (df['count'] / df['count'].sum()) * 100
+    df["percent"] = df["percent"].astype(int)
 
     # write out
     out = args.out if args.out else sys.stdout
