6dc2a53e0d3d9d33d67b3ad570348f1b4e24ae1f
Anil Yelam <ayelam@vmware.com>
2023-01-11 17:32:24 -0800

    removing unused repos

--- apps/silo/config.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#define CONFIG_MAX_EVENTS 1
-
-/* TODO: should specify a number of threads to declare each fd */
-#define CONFIG_REGISTER_FD_TO_ALL_EPOLLS 1
-
-#define CONFIG_USE_EPOLLEXCLUSIVE 0
