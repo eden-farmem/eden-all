56456bb213c8ec568c8c88bc059fda1d5751f617
wantonsolutions <stewbertgrant@gmail.com>
2023-01-17 17:49:26 -0800

    added plot for exp1

--- hotos/exp_001/exp1_plot.py
+++ hotos/exp_001/exp1_plot.py
@@ -20,12 +20,8 @@ def organize_files():
         file_dict[name][percent]=file
     return file_dict
 
-
-
-
 file_dict=organize_files()
-
-fig, ax = plt.subplots(1,1,figsize=(10, 3))
+fig, ax = plt.subplots(1,1,figsize=(6, 6))
 
 for name in file_dict:
     full = file_dict[name]["100"]
@@ -37,10 +33,19 @@ for name in file_dict:
 
     full_loc = full_df["loc"].values
     nf_loc = nf_df["loc"].values
-    ratio = [x/y for x,y in zip(full_loc,nf_loc)]
+    x_axis = nf_df["lmemp"].values
+    ratio = [float(x)/float(y) for x,y in zip(full_loc,nf_loc)]
+    print(full_loc)
+    print(nf_loc)
+    print(x_axis)
+    print(ratio)
 
-    ax.plot(ratio, label=name)
+    ax.plot(x_axis,ratio, label=name)
 
-plt.show()
+ax.set_ylabel("Ratio of all faults to 95th percentile faults")
+ax.set_xlabel("Local Memory Percentage")
+ax.legend()
+plt.tight_layout()
+plt.savefig("exp1.pdf")
 
 
