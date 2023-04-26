4460d8eda3e1375c39c50fc21b3fe5b90de779ca
Anil Yelam <ayelam@vmware.com>
2023-04-26 10:55:26 -0700

    dataframe sosp commit

--- apps/dataframe/faultgraph.sh
+++ apps/dataframe/faultgraph.sh
@@ -53,42 +53,50 @@ expdir=${DATADIR}/${RUNID}
 mkdir -p ${expdir}/traces
 tracesfolded=${expdir}/traces/000_000.txt
 
-# locate fault samples data
-allfaultsin=$(ls ${expdir}/fault-samples-*.out 2>/dev/null)
-if ! [[ $allfaultsin ]] || [ ! -f ${allfaultsin} ]; then 
-    echo "no fault-samples-*.out found at ${expdir}; did you enable FAULT_SAMPLER?"
-    exit 1
+if [ ! -f ${tracesfolded} ] || [[ $FORCE ]]; then
+    # locate fault samples data
+    allfaultsin=$(ls ${expdir}/fault-samples-*.out 2>/dev/null)
+    if ! [[ $allfaultsin ]] || [ ! -f ${allfaultsin} ]; then 
+        echo "no fault-samples-*.out found at ${expdir}; did you enable FAULT_SAMPLER?"
+        exit 1
+    fi
+
+    #locate procmaps file (note: this is needed to resolve addresses to symbols)
+    procmapflag=
+    procmaps=$(ls ${expdir}/procmaps-* 2>/dev/null | head -1)
+    if [ -f ${procmaps} ]; then 
+        procmapflag="-pm ${procmaps}"
+    fi
+
+    echo "Merging traces..."
+    if [ -f ${expdir}/run_start ]; then  stopt="-st $(cat ${expdir}/run_start)"; fi
+    if [ -f ${expdir}/run_end ]; then  etopt="-et $(cat ${expdir}/run_end)"; fi
+    echo "python3 ${ROOT_SCRIPTS_DIR}/parse_fltrace_samples.py -i ${allfaultsin} ${procmapflag} ${stopt} ${etopt} > ${tracesfolded}"
+    python3 ${ROOT_SCRIPTS_DIR}/parse_fltrace_samples.py -i ${allfaultsin} ${procmapflag} ${stopt} ${etopt} > ${tracesfolded}
 fi
 
-#locate procmaps file (note: this is needed to resolve addresses to symbols)
-procmapflag=
-procmaps=$(ls ${expdir}/procmaps-* 2>/dev/null | head -1)
-if [ -f ${procmaps} ]; then 
-    procmapflag="-pm ${procmaps}"
-fi
-
-if [ -f ${expdir}/run_start ]; then  stopt="-st $(cat ${expdir}/run_start)"; fi
-if [ -f ${expdir}/run_end ]; then  etopt="-et $(cat ${expdir}/run_end)"; fi
-echo "python3 ${ROOT_SCRIPTS_DIR}/parse_fltrace_samples.py -i ${allfaultsin} ${procmapflag} ${stopt} ${etopt} > ${tracesfolded}"
-python3 ${ROOT_SCRIPTS_DIR}/parse_fltrace_samples.py -i ${allfaultsin} ${procmapflag} ${stopt} ${etopt} > ${tracesfolded}
+srcdir=${SCRIPTDIR}/dataframe
+# srcdir=${SCRIPTDIR}/dataframe/app
+# LOCALIZE="-s ${srcdir} --local"
 
 # Make flame graph
 if [ ! -d ${FLAMEGRAPHDIR} ]; then 
     echo "Clone and update FLAMEGRAPHDIR to point to https://github.com/brendangregg/FlameGraph"
 fi
-python3 ${ROOT_SCRIPTS_DIR}/prepare_flame_graph.py -s ${SCRIPTDIR} -i ${tracesfolded} --nolib -o ${expdir}/flamegraph.dat
-python3 ${ROOT_SCRIPTS_DIR}/prepare_flame_graph.py -s ${SCRIPTDIR} -i ${tracesfolded} --nolib -z -o ${expdir}/flamegraph-zero.dat
-python3 ${ROOT_SCRIPTS_DIR}/prepare_flame_graph.py -s ${SCRIPTDIR} -i ${tracesfolded} --nolib -nz -o ${expdir}/flamegraph-nonzero.dat
+python3 ${ROOT_SCRIPTS_DIR}/prepare_flame_graph.py -s ${SCRIPTDIR} -i ${tracesfolded} ${LOCALIZE} --nolib -o ${expdir}/flamegraph.dat
+python3 ${ROOT_SCRIPTS_DIR}/prepare_flame_graph.py -s ${SCRIPTDIR} -i ${tracesfolded} ${LOCALIZE} --nolib -z -o ${expdir}/flamegraph-zero.dat
+python3 ${ROOT_SCRIPTS_DIR}/prepare_flame_graph.py -s ${SCRIPTDIR} -i ${tracesfolded} ${LOCALIZE} --nolib -nz -o ${expdir}/flamegraph-nonzero.dat
 ${ROOT_SCRIPTS_DIR}/flamegraph.pl ${expdir}/flamegraph.dat --color=fault --width=1600 --fontsize=12 > ${expdir}/flamegraph.svg
 ${ROOT_SCRIPTS_DIR}/flamegraph.pl ${expdir}/flamegraph-zero.dat --title "${APPNAME} (Allocation Faults)" --color=fault --width=1600 --fontsize=12 > ${expdir}/flamegraph-zero.svg
 ${ROOT_SCRIPTS_DIR}/flamegraph.pl ${expdir}/flamegraph-nonzero.dat --color=fault --width=1600 --fontsize=12 > ${expdir}/flamegraph-nonzero.svg
 
 # Also dump locations in plain 
-srcdir=${SCRIPTDIR}/dataframe
-# srcdir=${SCRIPTDIR}/dataframe/app
-python3 ${ROOT_SCRIPTS_DIR}/prepare_flame_graph.py -s ${srcdir} -i ${tracesfolded} --plain --local -o ${expdir}/locations.dat
-python3 ${ROOT_SCRIPTS_DIR}/prepare_flame_graph.py -s ${srcdir} -i ${tracesfolded} --plain --local -z -o ${expdir}/locations-zero.dat
-python3 ${ROOT_SCRIPTS_DIR}/prepare_flame_graph.py -s ${srcdir} -i ${tracesfolded} --plain --local -nz -o ${expdir}/locations-nonzero.
+for zeroopt in "" "--zero" "--nonzero"; do
+    for localopt in "" "--local"; do
+        echo "OPTS: ${zeroopt} ${localopt}"
+        python3 ${ROOT_SCRIPTS_DIR}/prepare_flame_graph.py -s ${srcdir} -i ${tracesfolded} --plain ${zeroopt} ${localopt} -o ${expdir}/locations${zeroopt}${localopt}.dat
+    done
+done
 
 # remove samples data
 if [ ! -z "${REMOVE}" ]; then
--- apps/dataframe/plot.sh
+++ apps/dataframe/plot.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 # set -e
 
-# Fastswap plots
+# Dataframe plots
 
-PLOTEXT=png
+PLOTEXT=pdf
 SCRIPT_PATH=`realpath $0`
 SCRIPT_DIR=`dirname ${SCRIPT_PATH}`
 ROOTDIR="${SCRIPT_DIR}/../../"
 ROOT_SCRIPTS_DIR="${ROOTDIR}/scripts/"
 PLOTDIR=${SCRIPT_DIR}/plots
 DATADIR=${SCRIPT_DIR}/data
-BINFILE="${SCRIPT_DIR}/dataframe/build/bin/main"
-TMP_FILE_PFX=tmp_df_plot_
+TMP_FILE_PFX=tmp_dframe_plot_
 
 source ${ROOT_SCRIPTS_DIR}/utils.sh
 
@@ -60,41 +59,127 @@ fi
 
 mkdir -p $PLOTDIR
 
-# fault locations
+# plot 1: fswap vs eden vs aifm
 if [ "$PLOTID" == "1" ]; then
     plotdir=$PLOTDIR/$PLOTID
     mkdir -p $plotdir
-    files=
     plots=
+    files=
+    NORMALIZE=1
+    CORES=5
 
     ## data
-    exp=run-09-21-12-26-23
+    cfgname="all_systems"
+    for runcfg in "eden" "fswap" "aifm"; do
+        LABEL=
+        LS=
+        CMI=
 
-    # get fault data
-    datafile=$plotdir/fault_data_${exp}
-    if [[ $FORCE ]] || [ ! -f "$datafile" ]; then
-        expdir=${DATADIR}/$exp
-        kfaultsin=${expdir}/kona_fault_samples.out
-        if [ ! -f ${kfaultsin} ]; then 
-            echo "kona_fault_samples.out not found for ${app} at ${expdir}"
-            exit 1
+        case $runcfg in
+        "fswap")        pattern="04-16"; rmem=fastswap; backend=rdma; cores=1; tperc=1; evb=; desc="paper"; LS=solid; CMI=1; LABEL="Fastswap";;
+        "eden")         pattern="04-16"; rmem=eden-bh; backend=rdma; cores=1; tperc=1; evb=64; desc="paper";  LS=solid; CMI=1; LABEL="Eden";;
+        "aifm")         rmem=aifm; LS=solid; CMI=1; LABEL="AIFM";;
+        *)              echo "Unknown config"; exit;;
+        esac
+
+        # filter results
+        cfg=be${bkend}_cores${cores}_tperc${tperc}
+        label=$runcfg
+        datafile=$plotdir/data_${LABEL}
+        thr=$((cores*tperc))
+        descopt=
+        evbopt=
+        rmemopt=
+        rdopt=
+        if [[ $desc ]]; then descopt="-d=$desc";    fi
+        if [[ $evb ]];  then evbopt="-evb=$evb";    fi
+        if [[ $evp ]];  then evpopt="-evp=$evp";    fi
+        if [[ $rmem ]];  then rmemopt="-r=$rmem";   fi
+        if [[ $rdhd ]];  then rdopt="-rd=$rdhd";    fi
+        if [[ $FORCE ]] || [ ! -f "$datafile" ]; then
+            if [[ "$rmem" == "aifm" ]]; then
+                if [[ ! -f "$datafile" ]]; then
+                    echo "ERROR! Expecting AIFM data in $datafile"
+                    exit 1
+                fi
+            else
+                echo "LMem%,Time,TimeErr,Faults,FaultsErr,HitR,Count,System,Backend,EvP,EvB,CPU,Zipfs" > $datafile
+                # for memp in `seq 10 10 100`; do
+                for memp in 9 16 22 29 35 41 48 54 61 67 74 80 87 93 100; do
+                    tmpfile=${TMP_FILE_PFX}data
+                    rm -f ${tmpfile}
+                    echo bash ${SCRIPT_DIR}/show.sh -cs="$pattern" -be=$backend -c=$cores \
+                        -t=${thr} -be=${bkend} ${descopt} ${evbopt} ${rmemopt} ${evpopt} ${rdopt} -lmp=${memp}
+                    bash ${SCRIPT_DIR}/show.sh -cs="$pattern" -be=$backend -c=$cores -of=$tmpfile  \
+                        -t=${thr} -be=${bkend} ${descopt} ${evbopt} ${rmemopt} ${evpopt} ${rdopt} -lmp=${memp}
+                    cat $tmpfile
+                    tmean=$(csv_column_mean $tmpfile "Runtime")
+                    tstd=$(csv_column_stdev $tmpfile "Runtime")
+                    xnum=$(csv_column_count $tmpfile "Runtime")
+                    fmean=$(csv_column_mean $tmpfile "NetReads")
+                    fstd=$(csv_column_stdev $tmpfile "NetReads")
+                    echo ${memp},${tmean},${tstd},${fmean},${fstd},${hitrmean},${xnum},${rmem},${bkend},${evp},${evb},${cores},${zipfs} >> ${datafile}
+                done
+
+                # compute and add normalized throughput column
+                baseline=
+                case $rmem in
+                "fastswap")         baseline=60;;
+                "eden-bh")          baseline=73;;
+                "aifm")             baseline=654;;
+                *)                  echo "Unknown rmem"; exit;;
+                esac
+                timenorm=$(csv_column "$datafile" "Time" | awk '{ if($1) printf "%lf\n", ($1/'$baseline'); else print ""; }')
+                timenormerr=$(csv_column "$datafile" "TimeErr" | awk '{ if($1) printf "%lf\n", ($1/'$baseline'); else print ""; }')
+                echo -e "TimeNorm\n${timenorm}" > ${TMP_FILE_PFX}normtime
+                echo -e "TimeErrNorm\n${timeerrnorm}" > ${TMP_FILE_PFX}normtimeerr
+                paste -d, $datafile ${TMP_FILE_PFX}normtime ${TMP_FILE_PFX}normtimeerr > ${TMP_FILE_PFX}normdata
+                mv ${TMP_FILE_PFX}normdata $datafile
+            fi
         fi
 
-        if [ ! -f ${BINFILE} ]; then 
-            echo "binary not found at ${binfile}"
-            exit 1
-        fi
-        python3 ${ROOT_SCRIPTS_DIR}/parse_kona_faults.py -i ${kfaultsin} -b ${BINFILE} > ${datafile}
-    fi
+        label=${LABEL:-$runcfg}
+        ls=${LS:-solid}
+        cmi=${CMI:-1}
+        plots="$plots -d $datafile -l $label -ls $ls -cmi $cmi"
+        cat $datafile
+    done
 
-    plotname=${plotdir}/fault_locations_cdf_${exp}.${PLOTEXT}
+    #plot time
+    plotname=${plotdir}/dframe_overhead.${PLOTEXT}
     if [[ $FORCE_PLOTS ]] || [ ! -f "$plotname" ]; then
-        python3 ${ROOT_SCRIPTS_DIR}/plot.py -z cdf --pdfdata            \
-            -d ${datafile} -l "Dataframe"                               \
-            -xl "Faulting locations" -yc "count" -yl "CDF of faults"    \
-            --xmin 0 --xmax 100 --nomarker                              \
-            --size 4 2.5 -fs 12 -of $PLOTEXT -o $plotname
+        python3 ${ROOTDIR}/scripts/plot.py ${plots}         \
+            -yc "TimeNorm" -yl "Normalized Runtime"         \
+            -xc "LMem%" -xl "Local Memory (%)"              \
+            --size 5 4 -fs 12 -of $PLOTEXT -o $plotname
     fi
+    files="$files $plotname"
+
+    # # #plot total faults
+    # YLIMS="--ymin 0 --ymax 10"
+    # plotname=${plotdir}/dframe_faults.${PLOTEXT}
+    # if [[ $FORCE_PLOTS ]] || [ ! -f "$plotname" ]; then
+    #     python3 ${ROOTDIR}/scripts/plot.py ${plots}                     \
+    #         -yce "Faults" "FaultsErr" -yl "Faults (Millions)" --ymul 1e-6 ${YLIMS}       \
+    #         -xc "LMem%" -xl "Local Memory (%)"                          \
+    #         --size 4.5 3 -fs 12 -of $PLOTEXT -o $plotname
+    # fi
+    # files="$files $plotname"
+
+    # # Hit ratio
+    # YLIMS="--ymin 0 --ymax 100"
+    # plotname=${plotdir}/hitr_${cfg}.${PLOTEXT}
+    # if [[ $FORCE_PLOTS ]] || [ ! -f "$plotname" ]; then
+    #     python3 ${ROOTDIR}/scripts/plot.py ${plots}                     \
+    #         -yc "HitR" -yl "Hit Ratio %" ${YLIMS}                       \
+    #         -xc "LMem%" -xl "Local Mem (%)"                             \
+    #         --size 4.5 3 -fs 12 -of $PLOTEXT -o $plotname
+    # fi
+    # files="$files $plotname"
+
+    # Combine
+    plotname=${plotdir}/${cfgname}.$PLOTEXT
+    montage -tile 2x0 -geometry +5+5 -border 5 $files ${plotname}
     display ${plotname} &
 fi
 
